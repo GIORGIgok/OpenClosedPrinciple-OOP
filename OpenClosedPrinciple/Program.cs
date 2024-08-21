e7fa9b287fcb3f4dab1552270dc4856425ebdc68
user <user@WINDOWS-NE2D8Q2>
2024-08-21 23:34:03 +0400

    Add project files.

---
+++ OpenClosedPrinciple/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace OpenClosedPrinciple
+{
+    // A base class for shapes.
+    public abstract class Shape
+    {
+        public abstract double GetArea();
+    }
+
+    // New shapes can be added without changing the Shape class.
+    public class Circle : Shape
+    {
+        public double Radius { get; set; }
+
+        public override double GetArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
+    }
+
+    public class Square : Shape
+    {
+        public double Side { get; set; }
+
+        public override double GetArea()
+        {
+            return Side * Side;
+        }
+    }
+
+}
